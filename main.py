48639a41398d72c4c2cdffc779c9fc8d6507237b
Your Name <shubhamcyberr@gmail.com>
2026-03-26 13:42:32 +0530

    Created the Readme file

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import random
 0 for gun 
 '''
 computer = random.choice([-1, 1, 0])
-youstr = input("enter your choice:")
+youstr = input("enter your choice:").lower()
 youdict = {"s":1, "w":-1, "g":0}
 reversedict = {1:"snake", -1:"water", 0:"gun"}
+
+if youstr not in youdict:
+    print("Invalid choice. Please enter s, w, or g.")
+    exit()
 
 you = youdict[youstr]
 
@@ -20,23 +24,7 @@ if(computer==you):
     print("its draw")
 
 else:
-    if(computer==1 and you==1):
+    if (you == 1 and computer == 0) or (you == 0 and computer == -1) or (you == -1 and computer == 1):
         print("you win")
-
-    elif(computer==-1 and you==0):
-        print("you lose")
-
-    elif(computer==1 and you==-1):
-        print("you lose")
-
-    elif(computer==1 and you==0):
-        print("you win")
-
-    elif(computer==0 and you==-1):
-        print("you win")
-
-    elif(computer==0 and you==1):
-        print("you lose")
-
     else:
-        print("something went wrong")
+        print("you lose")
