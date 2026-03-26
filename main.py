c946bedcf18a813f49778264f64de037f1e4445d
Your Name <shubhamcyberr@gmail.com>
2026-03-26 13:28:11 +0530

    First Repo

---
+++ main.py
@@ -0,0 +1,42 @@
+import random 
+'''
+1 for snake 
+-1 for water 
+0 for gun 
+'''
+computer = random.choice([-1, 1, 0])
+youstr = input("enter your choice:")
+youdict = {"s":1, "w":-1, "g":0}
+reversedict = {1:"snake", -1:"water", 0:"gun"}
+
+you = youdict[youstr]
+
+# now we have 2 number. you and computer 
+
+print(f"you choose {reversedict[you]}\n computer choose {reversedict[computer ]}")
+
+
+if(computer==you):
+    print("its draw")
+
+else:
+    if(computer==1 and you==1):
+        print("you win")
+
+    elif(computer==-1 and you==0):
+        print("you lose")
+
+    elif(computer==1 and you==-1):
+        print("you lose")
+
+    elif(computer==1 and you==0):
+        print("you win")
+
+    elif(computer==0 and you==-1):
+        print("you win")
+
+    elif(computer==0 and you==1):
+        print("you lose")
+
+    else:
+        print("something went wrong")
